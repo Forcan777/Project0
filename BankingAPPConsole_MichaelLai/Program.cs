fadc06312457a14710014194d076c3f21a95e88e
Michael Lai <michael.c.lai@outlook.com>
2022-03-09 12:07:00 -0800

    Project 0 Initialized !!

---
+++ BankingAPPConsole_MichaelLai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingAPPConsole_MichaelLai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
